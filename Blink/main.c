0dc1449a28005f48744680dfb0ce6892292f8df0
Gabriel Fornaeus <gf@hax0r.se>
2012-04-08 06:15:47 +0200

    Changes
    Signed-off-by: Gabriel Fornaeus <gf@hax0r.se>

--- Blink/main.c
+++ Blink/main.c
@@ -1,17 +1,52 @@
 /*
- I can do code me
- aaa
+ Blink an LED
+ By Gabriel Fornaeus <gf@hax0r.se>
+
+
  */
 
 #include <avr/io.h>
 
+// Define functions
+//======================
+void ioinit(void); // Initialises IO
+void delay_ms(uint16_t x); // General purpose delay
+//======================
+
 int main(void)
 {
+  ioinit(); // Setup IO pins and defaults
+  while(1) {
+    PORTC = 0xFF;
+    PORTB = 0xFF;
+    PORTD = 0xFF;
+    delay_ms(500);
 
-    // Insert code
+    PORTC = 0x00;
+    PORTB = 0x00;
+    PORTD = 0x00;
+    delay_ms(500);
+  }
+  return 0;
+}
 
-    while(1)
-    ;
+void ioinit (void)
+{
+    //1 = output, 0 = input
+    DDRB = 0b11111111; //All outputs
+    DDRC = 0b11111111; //All outputs
+    DDRD = 0b11111110; //PORTD (RX on PD0)
+}
 
-    return 0;
+//General short delays
+void delay_ms(uint16_t x)
+{
+  uint8_t y, z;
+  for ( ; x > 0 ; x--){
+    for ( y = 0 ; y < 90 ; y++){
+      for ( z = 0 ; z < 6 ; z++){
+        asm volatile ("nop");
+      }
+    }
+  }
 }
