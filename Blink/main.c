9ce783c1b837786bcd01f9804087db4088046aaa
Gabriel Fornaeus <gf@hax0r.se>
2012-04-09 00:12:45 +0200

    Seems that delay isn't all bad. Reverting

--- Blink/main.c
+++ Blink/main.c
@@ -4,34 +4,48 @@
 
  */
 #include <avr/io.h>
-#include <util/delay.h>
 
 // Define functions
 //======================
 void ioinit(void); // Initialises IO
+void delay_ms(uint16_t x); // General purpose delay
 //======================
 
 int main(void)
 {
-        ioinit(); // Setup IO pins and defaults
-        while(1) {
-                PORTC = 0xFF;
-                PORTB = 0xFF;
-                PORTD = 0xFF;
-                _delay_ms(500);
+  ioinit(); // Setup IO pins and defaults
+  while(1) {
+    PORTC = 0xFF;
+    PORTB = 0xFF;
+    PORTD = 0xFF;
+    delay_ms(500);
 
-                PORTC = 0x00;
-                PORTB = 0x00;
-                PORTD = 0x00;
-                _delay_ms(500);
-        }
-        return 0;
+    PORTC = 0x00;
+    PORTB = 0x00;
+    PORTD = 0x00;
+    delay_ms(500);
+  }
+  return 0;
 }
 
 void ioinit (void)
 {
-        //1 = output, 0 = input
-        DDRB = 0b11111111; //All outputs
-        DDRC = 0b11111111; //All outputs
-        DDRD = 0b11111110; //PORTD (RX on PD0)
+    //1 = output, 0 = input
+    DDRB = 0b11111111; //All outputs
+    DDRC = 0b11111111; //All outputs
+    DDRD = 0b11111110; //PORTD (RX on PD0)
+}
+
+//General short delays
+void delay_ms(uint16_t x)
+
+{
+  uint8_t y, z;
+  for ( ; x > 0 ; x--){
+    for ( y = 0 ; y < 90 ; y++){
+      for ( z = 0 ; z < 6 ; z++){
+        asm volatile ("nop");
+      }
+    }
+  }
 }
