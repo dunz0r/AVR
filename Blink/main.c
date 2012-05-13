273323a3928257b4d657de2ca8e30eef6560ab2b
Gabriel Fornaeus <gf@hax0r.se>
2012-05-14 00:38:14 +0200

    Vim + makefiles. IDEs are in the way

--- Blink/main.c
+++ Blink/main.c
@@ -1,51 +1,33 @@
-/*
- Blinks an LED
- By Gabriel Fornaeus <gf@hax0r.se>
+#define F_CPU 100000UL /* 1Mhz internal Oscillator */
 
- */
 #include <avr/io.h>
+#include <util/delay.h>
+#include <inttypes.h>
 
-// Define functions
-//======================
-void ioinit(void); // Initialises IO
-void delay_ms(uint16_t x); // General purpose delay
-//======================
-
-int main(void)
+/* Function for long delay */
+void delay_ms(uint16_t ms)
 {
-        ioinit(); // Setup IO pins and defaults
-        while(1) {
-                PORTC = 0xFF;
-                PORTB = 0xFF;
-                PORTD = 0xFF;
-                delay_ms(500);
-
-                PORTC = 0x00;
-                PORTB = 0x00;
-                PORTD = 0x00;
-                delay_ms(500);
-        }
-        return 0;
+    while (ms)
+    {
+        _delay_ms(1);
+        ms--;
+    }
 }
 
-void ioinit (void)
+int main (void)
 {
-        //1 = output, 0 = input
-        DDRB = 0b11111111; //All outputs
-        DDRC = 0b11111111; //All outputs
-        DDRD = 0b11111110; //PORTD (RX on PD0)
-}
+    /* PA0 is digital output */
+    DDRA = _BV (PA0);
 
-//General short delays
-void delay_ms(uint16_t x)
+    /* Loop forever */
+    while (1)
+    {
+        /* Clear PA0 on PORTA (digital high) and delay for one second */
+        PORTA &= ~_BV(PA0);
+        delay_ms(1000);
 
-{
-        uint8_t y, z;
-        for ( ; x > 0 ; x--) {
-                for ( y = 0 ; y < 90 ; y++) {
-                        for ( z = 0 ; z < 6 ; z++) {
-                                asm volatile ("nop");
-                        }
-                }
-        }
+        /* set PA0 on PORTA (digital low) and delay for one second */
+        PORTA |= _BV(PA0);
+        delay_ms(1000);
+    }
 }
