9432f7010e9c6aff723fa9a19eae4efa876e28e2
Gabriel Fornaeus <gf@hax0r.se>
2012-10-28 15:54:29 +0100

    Modifications to AD-thingies

--- MiniSumo_2012/adc.c
+++ MiniSumo_2012/adc.c
@@ -2,19 +2,47 @@
  * File Name : adc.c
  * Purpose : Init ADC and read ADC
  * Creation Date : 15-10-2012
- * Last Modified : mån 15 okt 2012 01:51:08
+ * Last Modified : sön 28 okt 2012 15:07:21
  * Created By : Gabriel Fornaeus, <gf@hax0r.se>
  *
  */
 
 #include "adc.h"
+
+// Global variables
+
+// adc_value is an array that contains all the AD values
+volatile unsigned char adc_value[NR_AD_CHANNEL];
+// Local value
+volatile unsigned char ad_count = 0;
+
 void init_adc(void)
 {
-	// Select Vref=AVCC
-	ADMUX |= (1<<REFS0);
-	// Set prescaler to 128 and enable ADC
-	ADCSRA |= (1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0)|(1<<ADEN);
+	// Select Vref=AVCC and left adjusted for 8-bit resolution
+	ADMUX |= (1<<REFS0) | (1<<ADLAR);
+	
+	// AD enable, AD interrupt enable and clk/128 prescaling
+	ADCSRA |= (1<<ADEN) | (1<<ADIE) | (1<<ADPS2) | (1<<ADPS1) | (1<<ADPS0);
+
+	// Prepares first AD channel
+	ADMUX &= 0b11111000;
+
+	// Starts the first conversion
+	ADCSRA |=(1<<ADSC);
 }
+
+// Interrupt that save the converted value to the array of values
+// and starts a new conversion
+int ISR(ADC_vect)
+{
+	adc_value[ad_count]=ADCH;
+
+	if(ad_count<(NR_AD_CHANNEL-1))
+		ad_count++;
+	else
+		ad_count=0;
+}
+
 
 uint16_t read_adc(uint8_t adc_channel)
 {
--- MiniSumo_2012/adc.h
+++ MiniSumo_2012/adc.h
@@ -2,7 +2,7 @@
  * File Name : adc.h
  * Purpose : Init and read ADC
  * Creation Date : 15-10-2012
- * Last Modified : mån 15 okt 2012 01:49:02
+ * Last Modified : sön 28 okt 2012 15:06:27
  * Created By : Gabriel Fornaeus, <gf@hax0r.se>
  *
  */
@@ -12,6 +12,13 @@
 #define ADC_H
 #include <stdio.h>
 #include <avr/io.h>
+
+#define NR_AD_CHANNEL 3
+// Global variables
+//
+// adc_value is an array that contains all the AD values
+volatile unsigned char adc_value[NR_AD_CHANNEL];
+
 
 void init_adc(void);
 uint16_t read_adc(uint8_t adc_channel);
