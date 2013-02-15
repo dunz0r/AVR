bd356441aa7d18020f35cc469fae5b8f110cbd41
Gabriel Fornaeus <gf@hax0r.se>
2013-02-15 03:18:09 +0100

    Placeholder functions

--- Minisumo2013/main.c
+++ Minisumo2013/main.c
@@ -2,7 +2,7 @@
  * File Name : main.c
  * Purpose : test adc
  * Creation Date : 2012-12-30
- * Last Modified : Fri 15 Feb 2013 03:09:41 CET
+ * Last Modified : Fri 15 Feb 2013 03:18:07 CET
  * Created By : Gabriel Fornaeus, <gf@hax0r.se>
  *
  */
@@ -35,7 +35,7 @@ uint8_t find_state(void) {
 	// Right sensor is above FAR_THRESH and left sensor is below
 	if(ad_value[1] > FAR_THRESH && ad_value[0] < FAR_THRESH)
 		state = 2;
-	// Right sensor is above NEAR_THRESH and left is below
+	// Left sensor is above NEAR_THRESH and right is below
 	if(ad_value[0] > NEAR_THRESH && ad_value[1] < NEAR_THRESH)
 		state = 3;
 	// Left sensor is above NEAR_THRESH and right is below
@@ -50,8 +50,30 @@ uint8_t find_state(void) {
 	return state;
 }
 
+void wander(void) {
+	printf("Wander\n");
+}
+
+void hunt_far_left(void) {
+	printf("Hunt far left\n");
+}
 
 
+void hunt_far_right(void) {
+	printf("Hunt far right\n");
+}
+
+void hunt_near_left(void) {
+	printf("Hunt near left\n");
+}
+
+void hunt_near_right(void) {
+	printf("Hunt near right\n");
+}
+
+void attack(void) {
+	printf("Attack\n");
+}
 
 int main(void) {
 	init_adc();
@@ -61,11 +83,31 @@ int main(void) {
 	// Assign our stream to standard I/O streams
 	stdout=&usart0_str;
 
-	// Enable interrupts
+	// Enable global interrupts
 	sei();
 
-	uint8_t state;
 	for(;;) {
-		state = find_state();
+		uint8_t state = find_state();
+		switch(state) {
+			case 1:
+				hunt_far_left();
+				break;
+			case 2:
+				hunt_far_right();
+				break;
+			case 3:
+				hunt_near_left();
+				break;
+			case 4:
+				hunt_near_right();
+				break;
+			case 5:
+				attack();
+				break;
+			case 6:
+				wander();
+				break;
+		}
+
 	}
 }
