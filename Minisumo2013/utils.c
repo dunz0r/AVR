326247647d9a169b5a09c44069dd336ee4abbaae
Gabriel Fornaeus <gf@hax0r.se>
2013-02-21 05:11:44 +0100

    Utility function: is_within_range

--- Minisumo2013/utils.c
+++ Minisumo2013/utils.c
@@ -2,17 +2,24 @@
  * File Name : constrain.c
  * Purpose : Various utilities
  * Creation Date : 2013-01-16
- * Last Modified : Wed 16 Jan 2013 21:30:36 CET
+ * Last Modified : tor 21 feb 2013 05:11:40
  * Created By : Gabriel Fornaeus, <gf@hax0r.se>
  *
  */
 
 #include <stdint.h>
 #include "utils.h"
-/*{{{ Subtracts and adds with overflow protection */
-int16_t constrain(int16_t high, int16_t  low, int16_t  x) {
-	if(x > high) x = high;
-	if(x < low) x = low;
-	return x;
+/*{{{ Constrains with overflow protection */
+int16_t constrain(int16_t high, int16_t  low, int16_t  value) {
+	if(value > high) value = high;
+	if(value < low) value = low;
+	return value;
+}
+
+char is_within_range(int16_t high, int16_t  low, int16_t  value) {
+	char output;
+	if(value < high && value > low) output = 1;
+	else output = 0;
+	return output;
 }
 /*}}}*/
