6b61eb2b4d9848f8154921646eebaca3d9b52a72
Gabriel Fornaeus <gf@hax0r.se>
2013-02-25 14:33:54 +0100

    Changes to main

--- Minisumo2013/main.c
+++ Minisumo2013/main.c
@@ -3,9 +3,9 @@
  * Purpose : test adc
  * Creation Date : 2012-12-30
 <<<<<<< HEAD
- * Last Modified : ons 20 feb 2013 18:37:08
+ * Last Modified : ons 20 feb 2013 19:17:55
 =======
- * Last Modified : ons 20 feb 2013 18:37:08
+ * Last Modified : ons 20 feb 2013 19:17:55
 >>>>>>> 7bba52c3ef81a39893ce5df0da030eaa58a8f217
  * Created By : Gabriel Fornaeus, <gf@hax0r.se>
  *
@@ -37,25 +37,25 @@ uint8_t find_state(void) {
 	if(ad_value[0] > ATT_THRESH || ad_value[1] > ATT_THRESH)
 		state = 1;
 	// Left sensor is above NEAR_THRESH and right is below
-	if(ad_value[0] > NEAR_THRESH && ad_value[1] < NEAR_THRESH)
+	else if(ad_value[0] > NEAR_THRESH && ad_value[1] < NEAR_THRESH)
 		state = 2;
 	// Right sensor is above NEAR_THRESH and left is below
-	if(ad_value[1] > NEAR_THRESH && ad_value[0] < NEAR_THRESH)
+	else if(ad_value[1] > NEAR_THRESH && ad_value[0] < NEAR_THRESH)
 		state = 3;
 	// Both sensors are above NEAR_THRESH
-	if(ad_value[1] > NEAR_THRESH && ad_value[0] > NEAR_THRESH)
+	else if(ad_value[1] > NEAR_THRESH && ad_value[0] > NEAR_THRESH)
 		state = 4;
 	// Left sensor is above FAR_THRESH and right sensor is below
-	if(ad_value[0] > FAR_THRESH && ad_value[1] < FAR_THRESH)
+	else if(ad_value[0] > FAR_THRESH && ad_value[1] < FAR_THRESH)
 		state = 5;
 	// Right sensor is above FAR_THRESH and left sensor is below
-	if(ad_value[1] > FAR_THRESH && ad_value[0] < FAR_THRESH)
+	else if(ad_value[1] > FAR_THRESH && ad_value[0] < FAR_THRESH)
 		state = 6;
 	// Both sensors are above FAR_THRESH
-	if(ad_value[1] > FAR_THRESH && ad_value[0] > FAR_THRESH)
+	else if(ad_value[1] > FAR_THRESH && ad_value[0] > FAR_THRESH)
 		state = 7;
 	// None of the sensors are above any threshhold
-	if(ad_value[0] < FAR_THRESH && ad_value[1] < FAR_THRESH)
+	else if(ad_value[0] < FAR_THRESH && ad_value[1] < FAR_THRESH)
 		state = 8;
 	return state;
 }
@@ -107,39 +107,53 @@ int main(void) {
 		switch(state) {
 			case 1:
 				printf("Attack\t0: %i 1: %i\n", ad_value[0], ad_value[1]);
-				_delay_ms(500);
+				set_heading(FULL_SPEED, 0);
+				_delay_ms(100);
 				break;
 			case 2:
 				printf("Hunt near left\t0: %i 1: %i\n", ad_value[0], ad_value[1]);
-				_delay_ms(500);
-			//	hunt_far_right();
+				//_delay_ms(500);
+				set_heading(BASE_SPEED, -190);
+				_delay_ms(100);
 				break;
 			case 3:
 				printf("Hunt near right\t0: %i 1: %i\n", ad_value[0], ad_value[1]);
-				_delay_ms(500);
+				set_heading(BASE_SPEED, 190);
+				_delay_ms(100);
+				//_delay_ms(500);
 			//	hunt_near_left();
 				break;
 			case 4:
 				printf("Hunt near both\t0: %i 1: %i\n", ad_value[0], ad_value[1]);
-				_delay_ms(500);
+				set_heading(BASE_SPEED, 0);
+				_delay_ms(100);
+				//_delay_ms(500);
 			//	hunt_near_right();
 				break;
 			case 5:
 				printf("Hunt far left\t0: %i 1: %i\n", ad_value[0], ad_value[1]);
-				_delay_ms(500);
+				set_heading(BASE_SPEED, -60);
+				_delay_ms(100);
+				//_delay_ms(500);
 			//	attack();
 				break;
 			case 6:
+				set_heading(BASE_SPEED, 60);
 				printf("Hunt far right\t0: %i 1: %i\n", ad_value[0], ad_value[1]);
-				_delay_ms(500);
+				_delay_ms(100);
+				//_delay_ms(500);
 				break;
 			case 7:
+				set_heading(BASE_SPEED, 0);
 				printf("Hunt far both\t0: %i 1: %i\n", ad_value[0], ad_value[1]);
-				_delay_ms(500);
+				_delay_ms(100);
+				//_delay_ms(500);
 				break;
 			case 8:
+				set_heading(BASE_SPEED, 30);
 				printf("Search\t0: %i 1: %i\n", ad_value[0], ad_value[1]);
-				_delay_ms(500);
+				_delay_ms(100);
+				//_delay_ms(500);
 				break;
 		}
 
