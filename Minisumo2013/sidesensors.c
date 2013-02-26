d365093557eab4c7e137f57c2fb0ffe0c4ea99a6
Gabriel Fornaeus <gf@hax0r.se>
2013-02-26 16:47:08 +0100

    Sidesensor files

---
+++ Minisumo2013/sidesensors.c
@@ -0,0 +1,8 @@
+/*
+ * File Name : sidesensors.c
+ * Purpose : Read the digital sharps at the sides of the robot
+ * Creation Date : 26-02-2013
+ * Last Modified :
+ * Created By : Gabriel Fornaeus, <gf@hax0r.se>
+ *
+ */
