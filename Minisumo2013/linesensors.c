0c4acb658672289e186510480c7f1b91417ae8c9
Gabriel Fornaeus <gf@hax0r.se>
2013-03-23 11:03:55 +0100

    Test with ON_BLACK

--- Minisumo2013/linesensors.c
+++ Minisumo2013/linesensors.c
@@ -2,40 +2,67 @@
  * File Name : linesensors.c
  * Purpose : Linesensors etc
  * Creation Date : 30-01-2013
- * Last Modified : mån 18 mar 2013 22:57:21
+ * Last Modified : lör 23 mar 2013 11:03:47
  * Created By : Gabriel Fornaeus, <gf@hax0r.se>
  *
  */
 
+#include "constants.h"
 #include "linesensors.h"
 void init_linesensors() {
-		/* {{{ INT0 */
+	/* {{{ INT0 */
+	if(ON_BLACK){
 		// Set PD2 to be an input
 		DDRD &= ~(1 << PD2);
 		// Trigger on falling edge
 		// TODO test with any logic change
 		EICRA |= (1 << ISC01);
+		// Enable INT0
 		EIMSK |= (1 << INT0);
-		/* }}} */
-
-		/* {{{ INT1 */
-		// Set PD3 to be an input
+	} else {
+		// Set PD2 to be an input
 		DDRD &= ~(1 << PD2);
+
+		// Trigger on rising edge
+		// TODO test with any logic change
+		EICRA &= ~(1 << ISC01);
+		EICRA &= ~(1 << ISC00);
+		// Enable INT0
+		EIMSK |= (1 << INT0);
+	}
+
+	/* }}} */
+	/* {{{ INT1 */
+	if(ON_BLACK){
+		// Set PD3 to be an input
+		DDRD &= ~(1 << PD3);
 
 		// Trigger on falling edge
 		// TODO test with any logic change
 		EICRA |= (1 << ISC11);
+		// Enable INT1
 		EIMSK |= (1 << INT1);
-		/* }}} */
+	} else {
+		// Set PD3 to be an input
+		DDRD &= ~(1 << PD3);
+
+		// Trigger on rising edge
+		// TODO test with any logic change
+		EICRA &= ~(1 << ISC11);
+		EICRA &= ~(1 << ISC10);
+		// Enable INT1
+		EIMSK |= (1 << INT1);
+	}
+	/* }}} */
 }
 
 ISR (INT0_vect) {
 	_delay_ms(1);
 	if(!(PIND & (1 << PD2))){
 		set_heading(-255,0);
-		_delay_ms(50);
+		_delay_ms(STATE_DELAY);
 		set_heading(0,100);
-		_delay_ms(40);
+		_delay_ms(STATE_DELAY);
 	}
 }
 
@@ -43,8 +70,8 @@ ISR (INT1_vect) {
 	_delay_ms(1);
 	if(!(PIND & (1 << PD3))){
 		set_heading(-255,0);
-		_delay_ms(50);
-		set_heading(0,100);
-		_delay_ms(40);
+		_delay_ms(STATE_DELAY);
+		set_heading(0,-100);
+		_delay_ms(STATE_DELAY);
 	}
 }
--- Minisumo2013/main.c
+++ Minisumo2013/main.c
@@ -3,9 +3,9 @@
  * Purpose : test adc
  * Creation Date : 2012-12-30
 <<<<<<< HEAD
- * Last Modified : mån 18 mar 2013 22:57:50
+ * Last Modified : lör 23 mar 2013 10:40:12
 =======
- * Last Modified : mån 18 mar 2013 22:57:50
+ * Last Modified : lör 23 mar 2013 10:40:12
 >>>>>>> 60260805c406807d406ca70e0bc26a862f03c711
  * Created By : Gabriel Fornaeus, <gf@hax0r.se>
  *
